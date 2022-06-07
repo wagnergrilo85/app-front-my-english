5cc912566182e449efe3f71abc16e60d526267c1
Wagner Grilo <wagner.grilo85@gmail.com>
2022-06-07 18:10:39 -0300

    criando filtro de busca de palavras

--- src/app/palavras/listar-palavras/listar-palavras.component.ts
+++ src/app/palavras/listar-palavras/listar-palavras.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PalavrasModel } from 'src/app/model/palavras.model';
+import { TipoPalavraModel } from 'src/app/model/tipo-palavra.model';
 import { PalavraService } from 'src/app/services/palavra.service';
+import { TipoPalavraService } from 'src/app/services/tipo-palavra.service';
 
 @Component({
   selector: 'app-listar-palavras',
@@ -9,14 +11,82 @@ import { PalavraService } from 'src/app/services/palavra.service';
 })
 export class ListarPalavrasComponent implements OnInit {
 
-  palavras: PalavrasModel[];
+  public showCardPesquisa: boolean = false;
+  public palavras: PalavrasModel[];
+  public tipoPalavras: Array<TipoPalavraModel> = [];
+  public arrayPalavrasBackupPequisa: Array<PalavrasModel> = [];
 
-  constructor(private palavrasService: PalavraService) { }
+  public objPesquisa: any = {
+    "direcao": "",
+    "tipoPalavra": TipoPalavraModel,
+    "tag": "",
+    "pesquisar": ""
+  }
+
+  constructor(
+    private palavrasService: PalavraService,
+    private tipoPalavraServise: TipoPalavraService,
+    ) {
+  }
 
   ngOnInit() {
     this.palavrasService.listarPalavras().subscribe(lista => {
       this.palavras = lista;
     });
+    this.getListaTipo();
   }
+
+  mostrarCardPesquisa(mostrar: any){
+    this.showCardPesquisa = mostrar;
+  }
+
+  getListaTipo(){
+    this.tipoPalavraServise.listarTipos().subscribe(respListaTipos =>{
+      this.tipoPalavras = respListaTipos;
+    });
+  }
+
+  pesquisar(){
+      this.arrayPalavrasBackupPequisa = this.palavras;
+
+      if(this.objPesquisa.direcao === "portugues"){
+        this.palavras = this.palavras.filter(p => p.palavraPt === this.objPesquisa.pesquisar );
+      }
+
+      if(this.objPesquisa.direcao === "ingles"){
+        this.palavras = this.palavras.filter(p => p.palavraEg === this.objPesquisa.pesquisar );
+      }
+
+      if(this.objPesquisa.direcao === "NA"){
+        this.palavras = this.palavras.filter(p => (p.palavraEg === this.objPesquisa.pesquisar) || (p.palavraPt === this.objPesquisa.pesquisar));
+      }
+
+      if(this.objPesquisa.tipoPalavra.nome !== ""){
+        this.palavras = this.palavras.filter(p => p.tipoPalavra.id === this.objPesquisa.tipoPalavra.id);
+      }
+
+      if(this.objPesquisa.tag !== ""){
+        this.palavras = this.palavras.filter(p => p.tag === this.objPesquisa.tag);
+      }
+
+
+      // this.palavras = this.palavras.filter(p => {
+
+      //   (this.objPesquisa.direcao === "portugues" ?
+      //     p.palavraPt === this.objPesquisa.pesquisar : this.objPesquisa.direcao === "ingles" ? p => p.palavraEg === this.objPesquisa.pesquisar : '')
+      //   && (this.objPesquisa.tipoPalavra.nome !== "" ? p => p.tipoPalavra.id === this.objPesquisa.tipoPalavra.id : '')
+      //   && (this.objPesquisa.tag !== "" ? p => p.tag === this.objPesquisa.tag : '')
+      // });
+
+      console.log(this.objPesquisa);
+      console.log(this.palavras);
+  }
+
+  ocultarPesquisar(){
+    this.palavras = this.arrayPalavrasBackupPequisa;
+    this.arrayPalavrasBackupPequisa = [];
+    this.showCardPesquisa = false;
+  }
+
 
 }
--- src/app/shared/card-top-listar/card-top-listar.component.ts
+++ src/app/shared/card-top-listar/card-top-listar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,8 +14,9 @@ export class CardTopListarComponent implements OnInit {
   @Input() rotaNovaEntidade: string = "";
   @Input() rotaListaCategorias: string = "";
   @Input() rotaListaTipos: string = "";
-  @Input() rotaFiltrar: string = "";
+  @Input() rotaFiltrar: boolean;
   @Input() rotaImprimir: string = "";
+  @Output() mostrarCardPesquisa = new EventEmitter();
 
   ngOnInit() {
   }
@@ -36,7 +37,7 @@ export class CardTopListarComponent implements OnInit {
     this._router.navigateByUrl(this.rotaImprimir);
   }
 
-  fnRotaFiltrar(){
-    this._router.navigateByUrl(this.rotaFiltrar);
+  mostrarPesquisa(){
+    this.mostrarCardPesquisa.emit(true);
   }
 }
